e715fcd0400541556aef5fafc1f2337583254bce
Makarov Anton <antoxam07@mail.ru>
2018-03-28 13:00:14 +0300

    Added custom logger

---
+++ TravelGuide/Helpers/Extensions/Foundation.swift
@@ -0,0 +1,18 @@
+//
+//  Foundation.swift
+//  TravelGuide
+//
+//  Created by Anton Makarov on 28/03/2018.
+//  Copyright © 2018 Anton Makarov. All rights reserved.
+//
+
+import Foundation
+
+// Date
+extension Foundation.Date {
+    func getCurrentTime() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd/HH:mm:ss.SSS"
+        return dateFormatter.string(from: self)
+    }
+}
