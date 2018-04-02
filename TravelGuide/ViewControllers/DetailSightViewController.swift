210eb51a575d9fcb060f71d72a8dc139a32e6304
Makarov Anton <antoxam07@mail.ru>
2018-04-02 23:56:22 +0300

    Added detail sights screen (UI)

---
+++ TravelGuide/ViewControllers/DetailSightViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailSightViewController.swift
+//  TravelGuide
+//
+//  Created by Anton Makarov on 02/04/2018.
+//  Copyright © 2018 Anton Makarov. All rights reserved.
+//
+
+import UIKit
+
+class DetailSightViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
